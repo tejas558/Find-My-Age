a5708f18671458d58bf87184e1cd94d95408c5aa
Tejas Mahajan <88541385+ttorpy@users.noreply.github.com>
2022-08-05 23:31:28 +0900

    final

--- app/src/main/java/com/t558/findmyage/MainActivity.kt
+++ app/src/main/java/com/t558/findmyage/MainActivity.kt
@@ -2,10 +2,24 @@ package com.t558.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val BNfindAge = findViewById<Button>(R.id.BNfindAge)
+        val ETbirthYear = findViewById<EditText>(R.id.ETbirthYear)
+        val TVshowAge = findViewById<TextView>(R.id.TVshowAge)
+        BNfindAge.setOnClickListener {
+            var year = ETbirthYear.text.toString().toInt()
+            var currYear = Calendar.getInstance().get(Calendar.YEAR)
+            var displayYear = currYear - year
+            TVshowAge.text = "Your age is $displayYear"
+        }
     }
 }
